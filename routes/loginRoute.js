89036bc6fed6ea99fe509c69adc1e5d331ab9609
Lydia <leedush21@gmail.com>
2018-12-10 22:54:45 +0200

    feat: Feature 16 login route

---
+++ routes/loginRoute.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+//const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+// Load input validation
+const validateRegisterInput = require('../utils/validators').register;
+
+const router = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+/**
+ * @route   POST api/auth/login
+ * @desc    Login User
+ * @access  Public
+ */
+router.post('/login', 'jwt', { session: false }, (req, res) => {
+	const { errors, isValid } = validateRegisterInput(req.body);
+
+	// Validate request body
+	if (!isValid) return res.status(400).json(errors);
+	return User.findOne({
+		email: req.body.email,
+	}).then(user => {
+		if (!user) {
+			return res.status(400).json({ message: 'Incorect user' });
+		} else {
+			bcrypt.compare(req.body.password, user.password, (err, res) => {
+				if (res === true) {
+					return res.status(200).json({ message: 'Login Successful' });
+				} else {
+					return res.status(400).json({ message: 'Incorect password' });
+				}
+			});
+		}
+	});
+});
+
+module.exports = router;
