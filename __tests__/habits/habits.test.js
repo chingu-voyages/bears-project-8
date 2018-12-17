8557af7563cd207d1e179eee78c6656fb0b38b94
arielbk <buchwald.kearney@gmail.com>
2018-12-17 16:39:46 +0100

    feat: Add habit test, sending only user id

--- __tests__/habits/habits.test.js
+++ __tests__/habits/habits.test.js
@@ -33,6 +33,17 @@ describe('API - Habit', () => {
 	});
 
 	describe('Habit - Create', () => {
+		test('If no data is passed a 400 status should be returned with error', done =>
+			request
+				.post('/api/habit/create')
+				.send({ user: '5c0cee8d8c452f13fbfe2281' })
+				.end((err, res) => {
+					if (err) throw err;
+					expect(res.status).toBe(400);
+					expect(res.body.name).toBe('Habit name field is required');
+					done();
+				}));
+
 		test('If correct data is passed new habit should be created', done =>
 			request
 				.post('/api/habit/create')
