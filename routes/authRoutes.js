12b0953fa65fbd05f7a7770017387ed13d7480f9
Lydia <leedush21@gmail.com>
2018-12-19 15:20:17 +0200

    feat:login route finished

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ const validateLoginInput = require('../utils/validators').login;
 const router = express.Router();
 const saltRounds = 10;
 const payload = {
-	id: User.id,
+	id: User._id,
 	name: User.name,
 	email: User.email,
 	avatar: User.avatar,
@@ -90,21 +90,28 @@ router.post('/login', (req, res) => {
 			if (!user) {
 				errors.email = 'User not found';
 				return res.status(404).json({ message: 'User not found', errors });
-			} else {
-				bcrypt.compare(req.body.password, user.password, (err, password) => {
-					if (err) {
-						return res
-							.status(401)
-							.json({
-								message: 'Unauthorized. Access denied to invalid credentials',
-							});
-					} else if (password === true) {
-						return payload;
-					}
-				});
 			}
-			const createToken = (user, secret, expiresIn) =>
-				jwt.sign({ token: createToken(payload, process.env.JWT_SECRET, '2h') });
+			bcrypt.compare(req.body.password, user.password, (err, result) => {
+				if (err) {
+					return res.status(401).json({
+						message: 'Unauthorized. Access denied to invalid credentials',
+					});
+				} else if (result) {
+					const token = jwt.sign(
+						{
+							payload,
+						},
+						process.env.JWT_SECRET,
+						{
+							expiresIn: '2h',
+						}
+					);
+					return res.status(200).json({
+						message: 'Auth successful',
+						token: token,
+					});
+				}
+			});
 		})
 		.catch(err => res.status(400).json(err));
 });
