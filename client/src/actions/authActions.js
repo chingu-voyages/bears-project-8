be7fdabef658e738e942e25cce826eff38b553b8
arielbk <buchwald.kearney@gmail.com>
2019-01-08 19:09:20 +0100

    feat: Add redux auth actions

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { SET_CURRENT_USER, GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+
+// Register user
+export const registerUser = (userData, history) => dispatch =>
+	axios
+		.post('api/auth/register', userData)
+		.then(() => history.push('/login'))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
+
+// Login - get user token
+export const loginUser = userData => dispatch =>
+	axios
+		.post('api/auth/login', userData)
+		.then(res => {
+			// Save to localStorage
+			const { token } = res.data;
+			localStorage.setItem('jwtToken', token);
+			// Set token to auth header
+			setAuthToken(token);
+			// Decode token for user data
+			const decoded = jwt_decode(token);
+			// Set current user
+			dispatch(setCurrentUser(decoded));
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+	return {
+		type: SET_CURRENT_USER,
+		payload: decoded,
+	};
+};
+
+// Log the user out
+export const logoutUser = () => dispatch => {
+	// Remove token from localStorage
+	localStorage.removeItem('jwtToken');
+	// Remove auth header for furute requests
+	setAuthToken(null);
+	// Set current user to {} - this set isAuthenticated to false
+	dispatch(setCurrentUser({}));
+};
