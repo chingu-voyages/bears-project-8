2583709e8729de9aede8f2941ab423d4a0273838
arielbk <buchwald.kearney@gmail.com>
2019-01-09 15:16:06 +0100

    feat: Add store

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -48,7 +48,7 @@ export const loginUser = userData => dispatch =>
 export const logoutUser = () => dispatch => {
 	// Remove token from localStorage
 	localStorage.removeItem('jwtToken');
-	// Remove auth header for furute requests
+	// Remove auth header for future requests
 	setAuthToken(null);
 	// Set current user to {} - this set isAuthenticated to false
 	dispatch(setCurrentUser({}));
---
+++ client/src/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/combineReducers';
+
+const initialState = {};
+
+const store = createStore(
+	rootReducer,
+	initialState,
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
+
+export default store;
