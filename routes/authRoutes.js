5e2969e457c9270cf20db00c9a6ffdc7887621c4
Lydia <leedush21@gmail.com>
2019-01-26 22:57:47 +0200

    user routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const gravatar = require('gravatar');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 
 const User = require('../models/User');
 
---
+++ routes/userRoutes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const passport = require('passport');
+const validator = require('validator');
+const url = require('url');
+
+const router = express.Router();
+const User = require('../models/User');
+
+/**
+ * @route   PUT user/:id/update
+ * @desc    update  User (name+avatar)
+ * @access  Private
+ */
+router.put('/:id/edit', passport.authenticate('jwt', { session: false }), (req, res) =>
+	User.findById(req.params.id)
+		.then(user => {
+			if (!user) {
+				return res.status(404).json({ message: 'User not found' });
+			}
+			if (user._id.toHexString() !== req.user._id.toHexString()) {
+				return res.status(401).json({ message: 'Unauthorized' });
+			}
+
+			if (req.body.name) {
+				// Run validation for name
+				user.name = req.body.name;
+			}
+			// image URL validation
+			if (
+				req.body.imgUrl ===
+				validator.isURL(url, {
+					protocols: ['http', 'https'],
+					require_protocol: true,
+					require_tld: true,
+				})
+			) {
+				user.avatar = req.body.imgUrl;
+			}
+
+			return user
+				.save()
+				.then(savedUser =>
+					res.status(200).json({ message: 'User updated successfully', user: savedUser })
+				)
+				.catch(err => res.status(400).json({ message: 'User not updated', err }));
+		})
+		.catch(err => res.status(400).json({ message: 'Bad request', err }))
+);
+
+/** 
+    User- delete route
+  user should be able to delete his profile 
+   @route   DELETE user/:id 
+ * @desc    delete User
+ * @access  Public  
+*/
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	User.findById(req.params.id)
+		.then(user => {
+			if (user.toHexString() !== req.user.id) {
+				return res.status(401).json({ message: 'Unauthorized' });
+			}
+			return user
+				.remove()
+				.then(() => res.status(200).json({ message: 'User deleted successfully' }));
+		})
+		.catch(() => res.status(404).json({ message: 'User not found' }));
+});
+
+module.exports = router;
