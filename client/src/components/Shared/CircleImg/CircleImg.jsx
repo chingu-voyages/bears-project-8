ec176230e6a04fbba719da78f54d83073c604ed7
Darkø Tasevski <darktasevski@gmail.com>
2019-01-28 09:16:02 +0100

    Feature/profile (#74)
    * feature/profile: Initial Profile setup
    
    * feature/profile: Add CircleImg component
    
    * feature/profile: Add User info section
    
    * feature/profile: Add Button component
    
    * feature/profile: Profile component done
    
    * fix: Minor fixes
    
    * feature/profile: Connect Profile to Redux
    
    * fix: typo
    
    * fix: test setup
    
    * fix: small change
    
    * fix: CR fixes

---
+++ client/src/components/Shared/CircleImg/CircleImg.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Wrapper, Title, Subtitle, Image, PlusCircle } from './CircleImg.styled';
+
+const CircleImg = ({ type, size, imgUrl, title, subtitle, clickHandler }) => (
+	<Wrapper>
+		{title && <Title>{title}</Title>}
+		{type !== 'wPlus' ? (
+			<Image type={type} size={size} onClick={clickHandler} />
+		) : (
+			<PlusCircle size={size}>+</PlusCircle>
+		)}
+
+		{subtitle && <Subtitle>{subtitle}</Subtitle>}
+	</Wrapper>
+);
+
+CircleImg.propTypes = {
+	type: PropTypes.oneOf(['wPlus']).isRequired,
+	imgUrl: PropTypes.string,
+	title: PropTypes.string,
+	subtitle: PropTypes.string,
+	clickHandler: PropTypes.func.isRequired,
+	size: PropTypes.string,
+};
+
+CircleImg.defaultProps = {
+	imgUrl: '',
+	title: '',
+	subtitle: '',
+	size: 'small',
+};
+
+export default CircleImg;
