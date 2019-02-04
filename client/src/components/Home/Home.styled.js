9c8bfa7dc554fd6a8fc9fccd8569ea56b3a69b90
arielbk <buchwald.kearney@gmail.com>
2019-02-05 00:28:10 +0100

    style: Second viewheight page

--- client/src/components/Home/Home.styled.js
+++ client/src/components/Home/Home.styled.js
@@ -9,12 +9,13 @@ export const FirstPage = styled.div`
 	width: 100%;
 	min-height: 100vh;
 	background: linear-gradient(15deg, #e8e8e8, #ffffff 80%);
-	padding-top: 160px;
+	padding: 160px 2rem 4rem;
 `;
 
 export const HomeNav = styled.div`
 	position: absolute;
 	top: 0;
+	left: 0;
 	height: 100px;
 	width: 100%;
 	display: flex;
@@ -59,7 +60,7 @@ export const NavLink = styled(Link)`
 	margin-left: 2rem;
 `;
 
-export const MainTitle = styled.h1`
+export const LargeTitle = styled.h1`
 	color: #5e5e5e;
 	font-size: 2.25rem;
 	font-weight: 600;
@@ -70,7 +71,7 @@ export const LargeLogo = styled.svg`
 	padding: 3rem 0;
 `;
 
-export const MainSubtitle = styled.div`
+export const LargeSubtitle = styled.div`
 	font-size: 1.5rem;
 	color: #878787;
 `;
@@ -88,5 +89,62 @@ export const TopRegisterButton = styled.button`
 	&:hover {
 		background: #bbead9;
 		color: #323232;
+	}
+`;
+
+export const SmallLink = styled.a`
+	text-decoration: none;
+	color: #5e5e5e;
+	font-size: 0.8rem;
+`;
+
+export const MiddleSection = styled.div`
+	width: 100%;
+	min-height: 100vh;
+	background: #f8f8f8;
+	padding: 5rem 2rem 0;
+
+	${LargeTitle} {
+		max-width: 600px;
+		margin: 0 auto;
+	}
+
+	${LargeSubtitle} {
+		margin: 3rem 0 0 0;
+	}
+
+	${SmallLink} {
+		display: inline-block;
+		margin: 1rem 0;
+		color: #c4c4c4;
+	}
+`;
+
+export const HabitStages = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
+	max-width: 860px;
+	padding: 4rem 0;
+	margin: 0 auto;
+`;
+
+export const StyledIcon = styled.svg`
+	width: 99px;
+	height: 99px;
+`;
+
+export const HabitStage = styled.div`
+	width: 170px;
+	h3 {
+		color: #5e5e5e;
+		font-weight: 400;
+		text-align: center;
+		padding: 1.5rem 0;
+	}
+	p {
+		font-size: 0.9rem;
+		color: #878787;
+		line-height: 1.75;
 	}
 `;
