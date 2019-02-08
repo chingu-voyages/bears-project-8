9d76daa31a2c430c46ef5daa514443ec68c6d9fe
Lydia <leedush21@gmail.com>
2019-02-08 19:50:41 +0200

    feat: goals route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const passport = require('passport');
 const validator = require('validator');
-const url = require('url');
 
 const router = express.Router();
 const User = require('../models/User');
@@ -17,7 +16,6 @@ router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
 			if (!user) {
 				return res.status(404).json({ message: 'User not found' });
 			}
-
 			if (user._id.toHexString() !== req.user._id.toHexString()) {
 				return res.status(401).json({ message: 'Unauthorized' });
 			}
@@ -39,9 +37,11 @@ router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
 					return res.status(400).json({ message: 'Bad image URL' });
 				}
 			}
-			// if (req.body.about) {
-			// 	user.about = about;
+
+			// if(req.user.about){
+			// 	user.about = req.user.about;
 			// }
+
 			return user
 				.save()
 				.then(savedUser =>
@@ -51,7 +51,34 @@ router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
 		})
 		.catch(err => res.status(400).json({ message: 'Bad request', err }))
 );
-
+/** 
+    User- goals route
+  user should be update goal to  his profile 
+   @route   GOALS user/:id/goals 
+ * @desc    Update user (goals)
+ * @access  Private
+*/
+router.put('/:id/goals', passport.authenticate('jwt', { session: false }), (req, res) =>
+	User.findById(req.params.id)
+		.then(user => {
+			if (!user) {
+				return res.status(404).json({ message: 'User not found' });
+			}
+			if (user._id.toHexString() !== req.user._id.toHexString()) {
+				return res.status(401).json({ message: 'Unauthorized' });
+			}
+			if (req.user.goals) {
+				user.goals = req.user.goals;
+			}
+			return user
+				.save()
+				.then(updated =>
+					res.status(200).json({ message: 'Goals updated successfully', user: updated })
+				)
+				.catch(err => res.status(400).json({ message: 'Goals not updated', err }));
+		})
+		.catch(err => res.status(400).json({ message: 'Bad request', err }))
+);
 /** 
     User- delete route
   user should be able to delete his profile 
