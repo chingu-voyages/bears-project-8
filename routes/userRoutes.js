d2ea1e39cbbe56c17035380038ab3af4ed65c1a1
Lydia <leedush21@gmail.com>
2019-02-09 22:58:41 +0200

    fix: deleted comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -52,34 +52,7 @@ router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
 		})
 		.catch(err => res.status(400).json({ message: 'Bad request', err }))
 );
-// /** 
-//     User- goals route
-//     user should be able to update goals to  his profile 
-//    @route   GOALS user/:id/goals 
-//  * @desc    Update user (goals)
-//  * @access  Private
-// */
-// router.put('/:id/goals', passport.authenticate('jwt', { session: false }), (req, res) =>
-// 	User.findById(req.params.id)
-// 		.then(user => {
-// 			if (!user) {
-// 				return res.status(404).json({ message: 'User not found' });
-// 			}
-// 			if (user._id.toHexString() !== req.user._id.toHexString()) {
-// 				return res.status(401).json({ message: 'Unauthorized' });
-// 			}
-// 			if (req.body.goals) {
-// 				user.goals = req.body.goals;
-// 			}
-// 			return user
-// 				.save()
-// 				.then(updated =>
-// 					res.status(200).json({ message: 'Goals updated successfully', user: updated })
-// 				)
-// 				.catch(err => res.status(400).json({ message: 'Goals not updated', err }));
-// 		})
-// 		.catch(err => res.status(400).json({ message: 'Bad request', err }))
-// );
+
 /** 
     User- delete route
   user should be able to delete his profile 
