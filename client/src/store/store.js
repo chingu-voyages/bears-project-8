7cad94e77aef4f134c63a5259e1804923ebbb36d
arielbk <buchwald.kearney@gmail.com>
2019-02-10 15:18:36 +0100

    fix: Fix react devtools typo

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -6,13 +6,6 @@ const initialState = {};
 
 // https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-	rootReducer,
-	initialState,
-	compose(
-		applyMiddleware(thunk),
-		composeEnhancers(applyMiddleware(thunk))
-	)
-);
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
