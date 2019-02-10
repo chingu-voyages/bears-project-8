c935aa3de64edcbf348ae03561c5e3fba3325f44
Darkø Tasevski <darko.tasevski@redbox.com>
2019-02-10 14:44:42 +0100

    fix: Fix fixture data

--- client/src/components/HabitList/HabitList.jsx
+++ client/src/components/HabitList/HabitList.jsx
@@ -5,10 +5,11 @@ import { Container, NoHabits } from './HabitList.styled';
 
 import Habit from '../Habit/Habit';
 
+// eslint-disable-next-line no-unused-vars
 const habitsTest = [
 	{
-		title: 'Test habit',
-		frequency: 'Once every day',
+		name: 'Test habit',
+		frequency: { period: 'Once every day' },
 		status: 'Due Tomorrow',
 		description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum',
 		type: 'Active',
@@ -17,8 +18,8 @@ const habitsTest = [
 		tags: ['office', 'exercise'],
 	},
 	{
-		title: 'Test habit 1',
-		frequency: 'Once every month',
+		name: 'Test habit 1',
+		frequency: { period: 'Once every month' },
 		status: 'Due',
 		description:
 			'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis.',
@@ -28,8 +29,8 @@ const habitsTest = [
 		tags: ['exercise'],
 	},
 	{
-		title: 'Test habit 2',
-		frequency: 'Once every week',
+		name: 'Test habit 2',
+		frequency: { period: 'Once every week' },
 		status: 'Due in 2 Days',
 		description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum',
 		type: 'Active',
@@ -38,8 +39,8 @@ const habitsTest = [
 		tags: ['office'],
 	},
 	{
-		title: 'Test habit 3',
-		frequency: 'Twice a week',
+		name: 'Test habit 3',
+		frequency: { period: 'Twice a week' },
 		status: 'Due',
 		description:
 			'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​.',
