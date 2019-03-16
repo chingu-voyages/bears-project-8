f0fdb650a051986590747b80fd68ebefba267531
Darkø Tasevski <darko.tasevski@redbox.com>
2019-03-16 17:20:46 +0100

    fix(server): Fix user tests

--- __tests__/user/user.test.js
+++ __tests__/user/user.test.js
@@ -25,6 +25,7 @@ describe('API - User', () => {
 				// Some prop juggling to avoid calling two endpoints here - ugly but necessary
 				body.id = body._id; // JWT decode expects id not _id
 				token = createToken(body, process.env.JWT_SECRET, '1h'); // create test token
+				user = body;
 				done();
 			});
 	});
@@ -45,96 +46,74 @@ describe('API - User', () => {
 	});
 
 	describe('User - Update', () => {
-		test('It should require authorization', () =>
-			request.put('/api/user/:id').then(response => {
-				expect(response.statusCode).toBe(401);
-			}));
+		test('It should require authorization', () => request.put('/api/user/:id').expect(401));
 
-		// test('If no data is passed a 400 status should be returned with error', done =>
-		//      user.name = req.body.name ;
-		// 	request
-		// 		.put(`/api/user/nodata`)
-		// 		.set('Authorization', token)
-		// 		.end((err, res) => {
-		// 			if (err) throw err;
-		// 			expect(res.status).toBe(400);
-		// 			expect(res.user.name).toBe('User name is required');
-		// 			done();
-		// 		}));
+		test('If no data is passed a 400 status should be returned with error', done => {
+			request
+				.put(`/api/user/${user.id}`)
+				.set('Authorization', token)
+				.end((err, res) => {
+					if (err) throw err;
 
-		// test('If an invalid user ID is passed a 404 error should be returned', done =>
-		// 	request
-		// 		.put('/api/user/badID')
-		// 		.set('Authorization', token)
-		// 		.send(userData)
-		// 		.end((err, res) => {
-		// 			if (err) throw err;
-		// 			expect(res.body.message).toBe('User not found');
-		// 			expect(res.status).toBe(404);
-		// 			done();
-		// 		}));
+					expect(res.status).toBe(400);
+					expect(res.body.message).toBe('No values to update');
+					done();
+				});
+		});
 
-		// 	test('If a valid user ID is passed, that user should be updated', done => {
-		// 		request
-		// 			.put(`/api/user/${userId}`)
-		// 			.set('Authorization', token)
-		// 			.send({
-		// 				id: '5c4e0f3cf936ff053883c599',
-		// 			})
-		// 			.end((err, res) => {
-		// 				if (err) throw err;
-		// 				expect(res.status).toBe(200);
-		// 				expect(res.name).toBe('User updated');
-		// 				expect(res.success).toBeTruthy();
-		// 				done();
-		// 			});
-		// 	});
-		// });
+		test('If an invalid user ID is passed a 404 error should be returned', done =>
+			request
+				.put(`/api/user/${mongoose.Types.ObjectId()}`)
+				.set('Authorization', token)
+				.end((err, res) => {
+					if (err) throw err;
 
-		// test('If a valid user ID is passed, that image should be updated', done => {
-		// 		request
-		// 			.put(`/api/user/${userId}`)
-		// 			.set('Authorization', token)
-		// 			.send({
-		// 				url:
-		// 			})
-		// 			.end((err, res) => {
-		// 				if (err) throw err;
-		// 				expect(res.status).toBe(200);
-		// 				expect(res.name).toBe('Image updated');
-		// 				expect(res.success).toBeTruthy();
-		// 				done();
-		// 			});
-		// 	});
-		// });
+					expect(res.body.message).toBe('User not found');
+					expect(res.status).toBe(404);
+					done();
+				}));
 
-		// describe('User - Delete', () => {
-		// 	test('It should require authorization', () =>
-		// 		request.delete('/api/user/:id').then(response => {
-		// 			expect(response.statusCode).toBe(401);
-		// 		}));
+		test('If a valid user ID is passed, that user should be updated', done => {
+			request
+				.put(`/api/user/${user.id}`)
+				.send({ imgUrl: 'https://test-img-url.jpg' })
+				.set('Authorization', token)
+				.expect(200)
+				.end((err, res) => {
+					if (err) throw err;
 
-		// test('If an invalid user ID is passed a 404 error should be returned', done =>
-		// 	request
-		// 		.delete('/api/user/nothing')
-		// 		.set('Authorization', token)
-		// 		.end((err, res) => {
-		// 			if (err) throw err;
-		// 			expect(res.status).toBe(404);
-		// 			expect(res.body.message).toBe('User not found');
-		// 			done();
-		// 		}));
+					expect(res.body.message).toBe('User updated successfully');
+					expect(res.body.user.avatar).toBe('https://test-img-url.jpg');
+					done();
+				});
+		});
+	});
 
-		// test('If a valid user ID is passed, that user should be deleted', done => {
-		// 	request
-		// 		.delete(`/api/user/${userId}`)
-		// 		.set('Authorization', token)
-		// 		.end((err, res) => {
-		// 			if (err) throw err;
-		// 			expect(res.status).toBe(200);
-		// 			expect(res.body.success).toBeTruthy();
-		// 			done();
-		// 		});
-		// });
+	describe('User - Delete', () => {
+		test('It should require authorization', () => request.delete('/api/user/:id').expect(401));
+
+		test('If an invalid user ID is passed a 404 error should be returned', done =>
+			request
+				.delete(`/api/user/${mongoose.Types.ObjectId()}`)
+				.set('Authorization', token)
+				.expect(404)
+				.end((err, res) => {
+					if (err) throw err;
+
+					expect(res.body.message).toBe('User not found');
+					done();
+				}));
+
+		test('If a valid user ID is passed, that user should be deleted', done =>
+			request
+				.delete(`/api/user/${user.id}`)
+				.set('Authorization', token)
+				.expect(200)
+				.end((err, res) => {
+					if (err) throw err;
+
+					expect(res.body.message).toBe('User deleted successfully');
+					done();
+				}));
 	});
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,34 +12,41 @@ const createToken = require('../utils/createToken');
  * @access  Private
  */
 router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) =>
-	User.findById(req.params.id)
-		.then(user => {
-			if (!user) {
-				return res.status(404).json({ message: 'User not found' });
-			}
-			if (user._id.toHexString() !== req.user._id.toHexString()) {
-				return res.status(401).json({ message: 'Unauthorized' });
-			}
-			// TODO: Run validation for name
-			// image URL validation
-			if (req.body.imgUrl) {
-				const validUrl = validator.isURL(req.body.imgUrl, {
-					protocols: ['http', 'https'],
-					require_protocol: true,
-					require_tld: true,
-				});
-				if (!validUrl)
-					return res.status(400).json({ imgUrl: 'Please enter a valid image URL' });
+	User.findById(req.params.id).then(user => {
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+		if (user._id.toHexString() !== req.user._id.toHexString()) {
+			return res.status(401).json({ message: 'Unauthorized' });
+		}
+		// DT TODO: Discuss how to handle no values provided on update request
+		// Should we just return existent user object or throw err?
+		if (!Object.keys(req.body).length > 0) {
+			return res.status(400).json({ message: 'No values to update' });
+		}
+
+		// TODO: Run validation for name
+		// image URL validation
+		if (req.body.imgUrl) {
+			const validUrl = validator.isURL(req.body.imgUrl, {
+				protocols: ['http', 'https'],
+				require_protocol: true,
+				require_tld: true,
+			});
+			if (!validUrl) {
+				return res.status(400).json({ imgUrl: 'Please enter a valid image URL' });
 			}
 
-			return { ...user, ...req.body }
-				.save()
-				.then(savedUser =>
-					res.status(200).json({ message: 'User updated successfully', user: savedUser })
-				)
-				.catch(err => res.status(400).json({ message: 'User not updated', err }));
-		})
-		.catch(err => res.status(400).json({ message: 'Bad request', err }))
+			user.avatar = req.body.imgUrl;
+		}
+
+		return user
+			.save()
+			.then(savedUser =>
+				res.status(200).json({ message: 'User updated successfully', user: savedUser })
+			)
+			.catch(err => res.status(400).json({ message: 'User not updated', err }));
+	})
 );
 
 /**
