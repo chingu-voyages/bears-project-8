3851e7e44348c3b2e89ef81faab9fdecca97278f
arielbk <buchwald.kearney@gmail.com>
2019-04-17 17:29:16 +0200

    refactor: DRY

--- client/src/actions/authActions.test.js
+++ client/src/actions/authActions.test.js
@@ -12,15 +12,15 @@ describe('registerUser action creator', () => {
 	});
 
 	const historyMock = { push: () => null };
+	const mockUser = {
+		name: 'Tester',
+		email: 'test@test.com',
+		avatar: 'https://avatar.com/avatar.gif',
+		password: 'password',
+	};
 
 	it('returns an error on failure', () => {
 		const store = createTestStore();
-		const mockUser = {
-			name: 'Tester',
-			email: 'test@test.com',
-			avatar: 'https://avatar.com/avatar.gif',
-			password: 'password',
-		};
 		const mockError = {
 			email: 'Email is already in use',
 		};
@@ -39,12 +39,6 @@ describe('registerUser action creator', () => {
 	});
 	it('registers user and returns no action (directly)', () => {
 		const store = createMockStore();
-		const mockUser = {
-			name: 'Tester',
-			email: 'test@test.com',
-			avatar: 'https://avatar.com/avatar.gif',
-			password: 'password',
-		};
 
 		moxios.wait(() => {
 			const request = moxios.requests.mostRecent();
@@ -70,13 +64,13 @@ describe('loginUser action creator', () => {
 	});
 
 	const historyMock = { push: () => null };
+	const mockUser = {
+		email: 'test@test.com',
+		password: 'password',
+	};
 
 	it('returns an error on failure', () => {
 		const store = createTestStore();
-		const mockUser = {
-			email: 'test@test.com',
-			password: 'password',
-		};
 		const mockError = {
 			email: 'A user with that email does not exist',
 		};
@@ -96,10 +90,6 @@ describe('loginUser action creator', () => {
 	});
 	it('logs user in on success', () => {
 		const store = createMockStore();
-		const mockUser = {
-			email: 'test@test.com',
-			password: 'password',
-		};
 		const mockToken =
 			'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYjc0MjQ2OWRhMjNhOWUzOWFiYzE1YyIsIm5hbWUiOiJ0ZXN0ZXIiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJhdmF0YXIiOiJodHRwczovLy8vd3d3LmdyYXZhdGFyLmNvbS9hdmF0YXIvYjY0MmI0MjE3YjM0YjFlOGQzYmQ5MTVmYzY1YzQ0NTI_cz0yMDAmcj1wZyZkPW1wIiwiYWJvdXQiOmZhbHNlLCJnb2FscyI6W10sImZyaWVuZHMiOltdLCJpYXQiOjE1NTU1MTM5MzUsImV4cCI6MTU1NTUxNzUzNX0.cIJjHzRYhguJmE9TrDn56o0ctKFH7IZYNIcpRBbim1Y';
 
