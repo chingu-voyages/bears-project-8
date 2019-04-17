cb9502ed0521c84b96572da70638d4f95308b774
arielbk <buchwald.kearney@gmail.com>
2019-04-17 17:22:00 +0200

    feat: Add redux-mock-store

--- client/src/utils/testUtils.js
+++ client/src/utils/testUtils.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import configureStore from 'redux-mock-store';
 import rootReducer from '../reducers/combineReducers';
 import { middleware } from '../store/store';
 
@@ -6,3 +7,5 @@ export const createTestStore = initialState => {
 	const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 	return createStoreWithMiddleware(rootReducer, initialState);
 };
+
+export const createMockStore = configureStore(middleware);
