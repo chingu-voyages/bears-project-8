ee60d4b492f7839b11d59dfe412002316c8b99f3
arielbk <buchwald.kearney@gmail.com>
2019-04-26 18:55:27 +0200

    refactor: Account for hours

--- client/src/utils/calculateDueDate.js
+++ client/src/utils/calculateDueDate.js
@@ -5,24 +5,37 @@ import endOfMonth from 'date-fns/end_of_month';
 import differenceInHours from 'date-fns/difference_in_hours';
 import differenceInDays from 'date-fns/difference_in_calendar_days';
 
+// TODO: account for when the habit was last logged, and the habit frequency
 export default frequency => {
 	const today = new Date();
 
 	switch (frequency) {
 		case 'Daily': {
 			const diff = differenceInHours(endOfDay(today), Date.now());
-
-			return `Due in ${diff} ${diff > 1 ? 'hours' : 'hour'}`;
+			const timeWord = diff > 1 ? 'hours' : 'hour';
+			return `Due in ${diff} ${timeWord}`;
 		}
 		case 'Weekly': {
-			const diff = differenceInDays(endOfWeek(today), Date.now());
-			// TODO: Handle case when the date is the last day due and difference should be calculated in hours instead of days
-			return `Due ${diff === 1 ? 'tomorrow' : diff === 0 ? 'today' : `in ${diff} days`}`;
+			let diff = differenceInDays(endOfWeek(today), Date.now());
+			let timeWord;
+			if (diff <= 1) {
+				diff = differenceInHours(endOfWeek(today), Date.now());
+				timeWord = diff > 1 ? 'hours' : 'hour';
+			} else {
+				timeWord = diff === 1 ? 'tomorrow' : diff === 0 ? 'today' : `in ${diff} days`;
+			}
+			return `Due ${timeWord}`;
 		}
 		case 'Monthly': {
-			const diff = differenceInDays(endOfMonth(today), Date.now());
-
-			return `Due ${diff === 1 ? 'tomorrow' : diff === 0 ? 'today' : `in ${diff} days`}`;
+			let diff = differenceInDays(endOfMonth(today), Date.now());
+			let timeWord;
+			if (diff <= 1) {
+				diff = differenceInHours(endOfMonth(today), Date.now());
+				timeWord = diff > 1 ? 'hours' : 'hour';
+			} else {
+				timeWord = diff === 1 ? 'tomorrow' : diff === 0 ? 'today' : `in ${diff} days`;
+			}
+			return `Due ${timeWord}`;
 		}
 		default:
 			return '';
